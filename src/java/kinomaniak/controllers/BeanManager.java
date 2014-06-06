369fd177b3ef963768897b652a5de9b1b7dda23d
qbassqbass <jakub.palac@gmail.com>
2014-06-07 01:08:52 +0200

    added Manager getters

--- src/java/kinomaniak/controllers/BeanManager.java
+++ src/java/kinomaniak/controllers/BeanManager.java
@@ -9,6 +9,7 @@ package kinomaniak.controllers;
 import java.util.ArrayList;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import javax.faces.bean.ManagedBean;
 import kinomaniak.beans.*;
 import kinomaniak.database.DBConnector;
 
@@ -16,8 +17,9 @@ import kinomaniak.database.DBConnector;
  *
  * @author Jakub
  */
-@Named(value = "beanManager")
+//@Named(value = "beanManager")
 @Dependent
+@ManagedBean
 public class BeanManager {
     DBConnector db;
     /**
