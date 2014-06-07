30dae9613af32c0c1e4d757d5887894655915fbb
qbassqbass <jakub.palac@gmail.com>
2014-06-07 12:18:33 +0200

    testing JSF

--- src/java/kinomaniak/controllers/BeanManager.java
+++ src/java/kinomaniak/controllers/BeanManager.java
@@ -30,6 +30,7 @@ public class BeanManager {
     ArrayList<Show> shows = new ArrayList<Show>();
     ArrayList<Ticket> tickets = new ArrayList<Ticket>();
     ArrayList<User> users = new ArrayList<User>();
+    ArrayList<CRoom> rooms = new ArrayList<CRoom>();
     
     Integer id = -1;
 
@@ -81,108 +82,140 @@ public class BeanManager {
     }
     
     public ArrayList<Attraction> getAttractions() {
-        return this.getAttractions(-1);
-    }
-    
-    public ArrayList<Attraction> getAttractions(int id){
-        ArrayList<Attraction> arr = new ArrayList<Attraction>();
         ArrayList<Object> a;
         for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Attraction") : db.parser.load(db.getConnection(), "Attraction", id)){
-            arr.add((Attraction)obj);
+            attractions.add((Attraction)obj);
         }
-        return arr;
+        return attractions;
     }
+    
+//    public ArrayList<Attraction> getAttractions(int id){
+//        ArrayList<Attraction> arr = new ArrayList<Attraction>();
+//        ArrayList<Object> a;
+//        for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Attraction") : db.parser.load(db.getConnection(), "Attraction", id)){
+//            arr.add((Attraction)obj);
+//        }
+//        return arr;
+//    }
     
     public ArrayList<CRoom> getRooms(){
-        return this.getRooms(-1);
-    }
-    
-    public ArrayList<CRoom> getRooms(int id){
-        ArrayList<CRoom> arr = new ArrayList<CRoom>();
         ArrayList<Object> a;
         for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "CRoom") : db.parser.load(db.getConnection(), "CRoom", id)){
-            arr.add((CRoom)obj);
+            rooms.add((CRoom)obj);
         }
-        return arr;
+        return rooms;
     }
+    
+//    public ArrayList<CRoom> getRooms(int id){
+//        ArrayList<CRoom> arr = new ArrayList<CRoom>();
+//        ArrayList<Object> a;
+//        for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "CRoom") : db.parser.load(db.getConnection(), "CRoom", id)){
+//            arr.add((CRoom)obj);
+//        }
+//        return arr;
+//    }
     
     public ArrayList<Product> getProducts(){
-        return this.getProducts(-1);
-    }
-    
-    public ArrayList<Product> getProducts(int id){
-        ArrayList<Product> arr = new ArrayList<Product>();
         ArrayList<Object> a;
         for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Product") : db.parser.load(db.getConnection(), "Product", id)){
-            arr.add((Product)obj);
+            products.add((Product)obj);
         }
-        return arr;
+        return products;
     }
+    
+//    public ArrayList<Product> getProducts(int id){
+//        ArrayList<Product> arr = new ArrayList<Product>();
+//        ArrayList<Object> a;
+//        for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Product") : db.parser.load(db.getConnection(), "Product", id)){
+//            arr.add((Product)obj);
+//        }
+//        return arr;
+//    }
     
     public ArrayList<Report> getReports(){
-        return this.getReports(-1);
-    }
-    
-    public ArrayList<Report> getReports(int id){
-        ArrayList<Report> arr = new ArrayList<Report>();
         ArrayList<Object> a;
         for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Report") : db.parser.load(db.getConnection(), "Report", id)){
-            arr.add((Report)obj);
+            reports.add((Report)obj);
         }
-        return arr;
+        return reports;
     }
+    
+//    public ArrayList<Report> getReports(int id){
+//        ArrayList<Report> arr = new ArrayList<Report>();
+//        ArrayList<Object> a;
+//        for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Report") : db.parser.load(db.getConnection(), "Report", id)){
+//            arr.add((Report)obj);
+//        }
+//        return arr;
+//    }
     
     public ArrayList<Res> getRes(){
-        return this.getRes(-1);
-    }
-    
-    public ArrayList<Res> getRes(int id){
-        ArrayList<Res> arr = new ArrayList<Res>();
         ArrayList<Object> a;
         for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Res") : db.parser.load(db.getConnection(), "Res", id)){
-            arr.add((Res)obj);
+            res.add((Res)obj);
         }
-        return arr;
+        return res;
     }
+    
+//    public ArrayList<Res> getRes(int id){
+//        ArrayList<Res> arr = new ArrayList<Res>();
+//        ArrayList<Object> a;
+//        for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Res") : db.parser.load(db.getConnection(), "Res", id)){
+//            arr.add((Res)obj);
+//        }
+//        return arr;
+//    }
     
     public ArrayList<Show> getShows(){
-        return this.getShows(-1);
-    }
-    
-    public ArrayList<Show> getShows(int id){
-        ArrayList<Show> arr = new ArrayList<Show>();
         ArrayList<Object> a;
         for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Show") : db.parser.load(db.getConnection(), "Show", id)){
-            arr.add((Show)obj);
+            shows.add((Show)obj);
         }
-        return arr;
+        return shows;
     }
+    
+//    public ArrayList<Show> getShows(int id){
+//        ArrayList<Show> arr = new ArrayList<Show>();
+//        ArrayList<Object> a;
+//        for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Show") : db.parser.load(db.getConnection(), "Show", id)){
+//            arr.add((Show)obj);
+//        }
+//        return arr;
+//    }
     
     public ArrayList<Ticket> getTickets(){
-        return this.getTickets(-1);
-    }
-    
-    public ArrayList<Ticket> getTickets(int id){
-        ArrayList<Ticket> arr = new ArrayList<Ticket>();
         ArrayList<Object> a;
         for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Ticket") : db.parser.load(db.getConnection(), "Ticket", id)){
-            arr.add((Ticket)obj);
+            tickets.add((Ticket)obj);
         }
-        return arr;
+        return tickets;
     }
+    
+//    public ArrayList<Ticket> getTickets(int id){
+//        ArrayList<Ticket> arr = new ArrayList<Ticket>();
+//        ArrayList<Object> a;
+//        for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "Ticket") : db.parser.load(db.getConnection(), "Ticket", id)){
+//            arr.add((Ticket)obj);
+//        }
+//        return arr;
+//    }
     
     public ArrayList<User> getUsers(){
-        return this.getUsers(-1);
-    }
-    
-    public ArrayList<User> getUsers(int id){
-        ArrayList<User> arr = new ArrayList<User>();
         ArrayList<Object> a;
         for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "User") : db.parser.load(db.getConnection(), "User", id)){
-            arr.add((User)obj);
+            users.add((User)obj);
         }
-        return arr;
+        return users;
     }
+    
+//    public ArrayList<User> getUsers(int id){
+//        ArrayList<User> arr = new ArrayList<User>();
+//        ArrayList<Object> a;
+//        for(Object obj : a = (id == -1) ? db.parser.load(db.getConnection(), "User") : db.parser.load(db.getConnection(), "User", id)){
+//            arr.add((User)obj);
+//        }
+//        return arr;
+//    }
     
 //    public ArrayList<Blaaa> getRooms(){
 //        return this.getRooms(-1);
